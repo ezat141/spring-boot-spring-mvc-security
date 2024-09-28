7a33d645400d0e88aae71ede0f101c0b35cd2253
Ezzat Mohamed <73354781+ezat141@users.noreply.github.com>
2024-09-29 00:14:43 +0300

    spring-boot-spring-mvc-security-default

---
+++ src/test/java/com/ebi/springbootspringsecurity/SpringBootSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebi.springbootspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
