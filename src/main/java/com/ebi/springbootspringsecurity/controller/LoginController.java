2a4c51148e8f7fc30bd1dfe9f1a9e6b313337f34
Ezzat Mohamed <73354781+ezat141@users.noreply.github.com>
2024-10-09 01:49:53 +0300

    user roles restrict access and custom-access-denied-page added

--- src/main/java/com/ebi/springbootspringsecurity/controller/LoginController.java
+++ src/main/java/com/ebi/springbootspringsecurity/controller/LoginController.java
@@ -10,5 +10,15 @@ public class LoginController {
 
 //        return "plain-login";
         return "fancy-login";
-        }
+    }
+
+    // add request mapping for /access-denied
+
+    @GetMapping("/access-denied")
+    public String showAccessDenied() {
+
+        return "access-denied";
+    }
+
+
 }
--- src/main/java/com/ebi/springbootspringsecurity/security/DemoSecurityConfig.java
+++ src/main/java/com/ebi/springbootspringsecurity/security/DemoSecurityConfig.java
@@ -37,11 +37,16 @@ public class DemoSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(configurer -> configurer.anyRequest().authenticated())
+        http.authorizeHttpRequests(configurer -> configurer
+                        .requestMatchers("/").hasRole("EMPLOYEE")
+                        .requestMatchers("/leaders/**").hasRole("MANAGER")
+                        .requestMatchers("/systems/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
                 .formLogin(form -> form.loginPage("/showMyLoginPage")
                         .loginProcessingUrl("/authenticateTheUser")
                         .permitAll())
-                .logout(LogoutConfigurer::permitAll);
+                .logout(LogoutConfigurer::permitAll)
+                .exceptionHandling(configurer -> configurer.accessDeniedPage("/access-denied"));
         return http.build();
 
     }
